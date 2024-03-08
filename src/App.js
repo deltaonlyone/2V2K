c3f07680fd784e8172f7e87e07daddc2baaa1a69
deltaonlyone <deltaonlyone@gmail.com>
2024-03-09 01:30:23 +0200

    added function to get users and their photos; styled with bootstrap

--- src/App.js
+++ src/App.js
@@ -3,132 +3,136 @@ import './styles/bootstrap-5.3.3-dist/css/bootstrap.min.css';
 import UsersList from './UsersList';
 import logo from './photos/Logo.svg';
 import randomAvatar from './photos/randomAvatar.jpg';
-import { searchOutline, homeOutline, folderOpenOutline, pieChartOutline, peopleOutline, chatbubblesOutline, settingsOutline  } from 'ionicons/icons';
-import { IonIcon } from '@ionic/react';
+import {
+    searchOutline,
+    homeOutline,
+    folderOpenOutline,
+    pieChartOutline,
+    peopleOutline,
+    chatbubblesOutline,
+    settingsOutline
+} from 'ionicons/icons';
+import {IonIcon} from '@ionic/react';
 import PhotoComponent from "./PhotoComponent";
+import AllUsersPhotos from "./AllUsersPhotos";
 
 const App = () => {
+    return (
+        <div className="App">
+            <nav className="bg-body-tertiary navbar-top-first">
+                <div className="container-fluid navbar-top-second">
+                    <form className="d-flex navbar-search-form" role="search">
+                        <input
+                            className="me-2 navbar-top-search"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        ></input>
+                    </form>
+
+                    <div className="navbar-top-right">
+                        <div className="navbar-item-inner-icon-wrapper flexbox icon-navbar-top d-none d-sm-flex">
+                            <IonIcon icon={folderOpenOutline}></IonIcon>
+                        </div>
+
+                        <div className="navbar-item-inner-icon-wrapper flexbox icon-navbar-top d-none d-sm-flex">
+                            <IonIcon icon={settingsOutline}></IonIcon>
+                        </div>
 
 
-  return (
+                        <div className="rounded-circle overflow-hidden navbar-mini-avatar">
+                            <img src={randomAvatar} alt="User" className="img-fluid"></img>
+                        </div>
+                    </div>
 
-      <div className="App">
-        <nav className="bg-body-tertiary navbar-top-first">
-          <div className="container-fluid navbar-top-second">
-            <form className="d-flex navbar-search-form" role="search">
-              <input
-                  className="me-2 navbar-top-search"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-              ></input>
-            </form>
-
-            <div className="navbar-top-right">
-              <div className="navbar-item-inner-icon-wrapper flexbox icon-navbar-top d-none d-sm-flex">
-                <IonIcon icon={folderOpenOutline}></IonIcon>
-              </div>
-
-              <div className="navbar-item-inner-icon-wrapper flexbox icon-navbar-top d-none d-sm-flex">
-                <IonIcon icon={settingsOutline}></IonIcon>
-              </div>
-
-
-              <div className="rounded-circle overflow-hidden navbar-mini-avatar">
-                <img src={randomAvatar} alt="User" className="img-fluid"></img>
-              </div>
-            </div>
-
-          </div>
-        </nav>
-
-
-
-        <nav id="navbar">
-          <ul className="navbar-items flexbox-col">
-            <li className="navbar-logo flexbox-left">
-              <a className="navbar-item-inner flexbox height-100">
-                <img className="height-100" src={logo} alt="OurLogo"></img>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={searchOutline}></IonIcon>
                 </div>
-                <span className="link-text">Search</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={homeOutline}></IonIcon>
-                </div>
-                <span className="link-text">Home</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={folderOpenOutline}></IonIcon>
-                </div>
-                <span className="link-text">Projects</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={pieChartOutline}></IonIcon>
-                </div>
-                <span className="link-text">Dashboard</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={peopleOutline}></IonIcon>
-                </div>
-                <span className="link-text">Team</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={chatbubblesOutline}></IonIcon>
-                </div>
-                <span className="link-text">Support</span>
-              </a>
-            </li>
-            <li className="navbar-item flexbox-left">
-              <a className="navbar-item-inner flexbox-left">
-                <div className="navbar-item-inner-icon-wrapper flexbox">
-                  <IonIcon icon={settingsOutline}></IonIcon>
-                </div>
-                <span className="link-text">Settings</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+            </nav>
 
 
-        <main id="main" className="flexbox-col">
+            <nav id="navbar">
+                <ul className="navbar-items flexbox-col">
+                    <li className="navbar-logo flexbox-left">
+                        <a className="navbar-item-inner flexbox height-100">
+                            <img className="height-100" src={logo} alt="OurLogo"></img>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={searchOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Search</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={homeOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Home</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={folderOpenOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Projects</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={pieChartOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Dashboard</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={peopleOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Team</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={chatbubblesOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Support</span>
+                        </a>
+                    </li>
+                    <li className="navbar-item flexbox-left">
+                        <a className="navbar-item-inner flexbox-left">
+                            <div className="navbar-item-inner-icon-wrapper flexbox">
+                                <IonIcon icon={settingsOutline}></IonIcon>
+                            </div>
+                            <span className="link-text">Settings</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 
-          <h2>Lorem ipsum!</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis,
-            rerum doloremque iste sed voluptates omnis molestias molestiae animi
-            recusandae labore sit amet delectus ad necessitatibus laudantium qui!
-            Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis!
-            Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur
-            assumenda. Obcaecati minima sed natus?
-          </p>
-          <div>
-            <UsersList/>
-            <PhotoComponent/>
-          </div>
-        </main>
-      </div>
-  );
+            <main id="main" className="flexbox-col">
+                <h2>Lorem ipsum!</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis,
+                    rerum doloremque iste sed voluptates omnis molestias molestiae animi
+                    recusandae labore sit amet delectus ad necessitatibus laudantium qui!
+                    Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis!
+                    Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur
+                    assumenda. Obcaecati minima sed natus?
+                </p>
+                <div>
+                    <UsersList/>
+                    {/*<PhotoComponent/>*/}
+                    <AllUsersPhotos/>
+                </div>
+            </main>
+        </div>
+    );
 };
 
 export default App;
