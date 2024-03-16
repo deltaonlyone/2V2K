f8d0e874d86784b3c77c1cf6a63fd6528987a059
StopRey <skillful02@gmail.com>
2024-03-16 23:57:40 +0200

    started new website making. New top navbar. Added mobile version of top navbar.

---
+++ src/NavbarExpanded.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import './NavbarTopCss.css';
+
+function NavbarExpanded(props) {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const handleToggle = () => {
+        setIsExpanded(!isExpanded);
+
+        const x = document.getElementsByClassName('container_top')[0];
+        x.classList.toggle("change");
+    };
+
+
+    return (
+        <nav className={`nav ${isExpanded ? 'nav--expanded' : ''}`}>
+            <a href="/" className="nav__brand">Snavvy</a>
+
+            <div className="nav__collapsable">
+
+                    <a href="#">Locations</a>
+                    <a href="#">Map</a>
+                    <a href="#">Photographers</a>
+
+
+                    <div className="toggle-switch">
+                        <label className="switch-label">
+                            <input type="checkbox" className="checkbox"></input>
+                            <span className="slider"></span>
+                        </label>
+                    </div>
+                    <div className="nav__cta">
+                        <a href="#" className="cta">Sign In</a>
+                    </div>
+
+            </div>
+
+            <button className="nav__collapser" onClick={handleToggle}>
+                <div className="container_top">
+                    <div id="bar1" className="bars"></div>
+                    <div id="bar2" className="bars"></div>
+                    <div id="bar3" className="bars"></div>
+                </div>
+
+            </button>
+        </nav>
+    );
+}
+
+export default NavbarExpanded;
