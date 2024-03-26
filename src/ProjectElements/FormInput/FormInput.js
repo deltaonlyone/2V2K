b33b088b7b996f6d3a4c5c8d5d34acfcfea32fd6
deltaonlyone <deltaonlyone@gmail.com>
2024-03-27 01:28:10 +0200

    connected with java api authorization function

--- src/ProjectElements/FormInput/FormInput.js
+++ src/ProjectElements/FormInput/FormInput.js
@@ -2,25 +2,31 @@ import React, {useState} from 'react';
 import styles from './FormInput.module.css';
 
 export function FormInput(props) {
+    const [inputValue, setInputValue] = useState('');
 
     const customStyle = {
         height: props.height,
         width: props.width,
+    };
 
-
-
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+        if (props.onChange) {
+            props.onChange(event.target.value);
+        }
     };
 
     return (
         <div className={styles['formInput']}>
-            <div className = {styles['TextInput']}>
+            <div className={styles['TextInput']}>
                 {props.text}
             </div>
-
-            <input className = {styles['fieldInput']} style={customStyle}>
-
-            </input>
-
+            <input
+                className={styles['fieldInput']}
+                style={customStyle}
+                value={inputValue}
+                onChange={handleChange}
+            />
         </div>
     );
 }
