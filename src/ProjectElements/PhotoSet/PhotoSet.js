c56ed6215fb96dc5a7b1d2131d2f2fdd8e087e2e
deltaonlyone <deltaonlyone@gmail.com>
2024-04-05 15:20:17 +0300

    fixed liner gradient for PhotoSet background

--- src/ProjectElements/PhotoSet/PhotoSet.js
+++ src/ProjectElements/PhotoSet/PhotoSet.js
@@ -7,11 +7,10 @@ import randomPhoto2 from './randomPhoto2.png';
 export function PhotoSet(props) {
 
 
-
     return (
         <nav className={styles.container}>
-            <div className = {styles['centerText']}>
-                <h1 className = {styles['topText']}>EXPLORE WIDE WORLD OF PHOTOGRAPHY WITH US</h1>
+            <div className={styles['centerText']}>
+                <h1 className={styles['topText']}>EXPLORE WIDE WORLD OF PHOTOGRAPHY WITH US</h1>
 
                 <div>
                     <input placeholder="email adress" type="text" name="text" className={styles['inputCenter']}></input>
@@ -19,29 +18,23 @@ export function PhotoSet(props) {
                 </div>
             </div>
 
-            <div className={styles['photoSetTop']}>
-                <img className = {styles['photoTop1']} src={randomPhoto1} alt="" />
-                <img className = {styles['photoTop2']} src={randomPhoto2} alt="" />
-                <img className = {styles['photoTop1']} src={randomPhoto2} alt="" />
-                <img className = {styles['photoTop2']} src={randomPhoto1} alt="" />
-                <img className = {styles['photoTop1']} src={randomPhoto1} alt="" />
+            <div className={styles['photoSet']}>
+                <div className={styles['photoSetTop']}>
+                    <img className={styles['photoTop1']} src={randomPhoto1} alt=""/>
+                    <img className={styles['photoTop2']} src={randomPhoto2} alt=""/>
+                    <img className={styles['photoTop1']} src={randomPhoto2} alt=""/>
+                    <img className={styles['photoTop2']} src={randomPhoto1} alt=""/>
+                    <img className={styles['photoTop1']} src={randomPhoto1} alt=""/>
+                </div>
+
+                <div className={styles['photoSetDown']}>
+                    <img className={styles['photoDown1']} src={randomPhoto1} alt=""/>
+                    <img className={styles['photoDown2']} src={randomPhoto2} alt=""/>
+                    <img className={styles['photoDown1']} src={randomPhoto2} alt=""/>
+                    <img className={styles['photoDown2']} src={randomPhoto1} alt=""/>
+                    <img className={styles['photoDown1']} src={randomPhoto2} alt=""/>
+                </div>
             </div>
-
-            <div className={styles['photoSetDown']}>
-                <img className = {styles['photoDown1']} src={randomPhoto1} alt="" />
-                <img className = {styles['photoDown2']} src={randomPhoto2} alt="" />
-                <img className = {styles['photoDown1']} src={randomPhoto2} alt="" />
-                <img className = {styles['photoDown2']} src={randomPhoto1} alt="" />
-                <img className = {styles['photoDown1']} src={randomPhoto2} alt="" />
-            </div>
-
-
-
-
-
-
-
-
 
 
         </nav>
