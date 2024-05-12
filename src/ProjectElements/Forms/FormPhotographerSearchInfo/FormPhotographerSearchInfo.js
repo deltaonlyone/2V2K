2e7c8c45e4ea4ddac255ffcb9ebbc81f1d11642c
StopRey <skillful02@gmail.com>
2024-05-13 00:32:35 +0300

    added form for photographer user search

--- src/ProjectElements/Forms/FormPhotographerSearchInfo/FormPhotographerSearchInfo.js
+++ src/ProjectElements/Forms/FormPhotographerSearchInfo/FormPhotographerSearchInfo.js
@@ -2,25 +2,92 @@ import React, {useState} from 'react';
 import styles from './FormPhotographerSearchInfo.module.css';
 
 import {useSelector} from 'react-redux';
+import {bindTrigger} from "material-ui-popup-state";
+import Button from "@mui/material/Button";
+import {FormButton} from "../FormButton/FormButton";
 
 export function FormPhotographerSearchInfo(props) {
     const currentTheme = useSelector(state => state.currentTheme);
 
+    const photos = props.photos; // Деструктуризація пропс
 
     return (
-        <div className = {`${styles['container']} ${currentTheme.backgroundColor}`}>
-            <div>
-                <a className={`${styles['userNameLocation']} ${currentTheme.backgroundColor}`}>
-
+        <div className={`${styles['container']} ${currentTheme.backgroundColor}`}>
+            <div className={`${styles['TopText']} `}>
+                <a className={`${styles['userName']} ${currentTheme.textColor}`}>
+                    {props.name}
                 </a>
 
-                <a className={`${styles['userLocation']} ${currentTheme.backgroundColor}`}>
-
+                <a className={`${styles['userLocation']} ${currentTheme.textGreyColorSecond}`}>
+                    {props.location}
                 </a>
             </div>
 
-            <div>
+            <div className={`${styles['mainElements']} `}>
+                <div className={`${styles['firstElements']} `}>
+                    <img src={props.avatar}
+                         alt={`AvatarUser`}
+                         className={`${styles['userAvatar']} `}
+                    />
+                </div>
 
+                <div className={`${styles['secondElements']} `}>
+                    <div className={`${styles['scrollContainer']} `}>
+                        {photos.map((photo, index) => (
+                            <img
+                                key={index}
+                                src={photo}
+                                alt={`Photo ${index}`}
+                                className={styles['photosStyle']}
+                            />
+                        ))}
+                    </div>
+
+
+                    <div className={`${styles['informationUserStyleDiv']} `}>
+                        <a className={`${styles['informationUserStyle']} ${currentTheme.textColor}`}>
+                            {props.informationUser}
+                        </a>
+                    </div>
+
+
+                    <div className={`${styles['priceElements']} `}>
+                        <a className={`${styles['priceHourStyle']} ${currentTheme.textColor}`}>
+                            {props.priceHour}
+                            $/hour
+                        </a>
+
+                        <a className={`${styles['minimumTimeStyle']} ${currentTheme.textColor}`}>
+                            Minimum {props.minimumTime} hours
+                        </a>
+                    </div>
+                </div>
+
+                <div className={`${styles['thirdElements']} `}>
+                    <div className={`${styles['buttonsStyle']} `}>
+                        <FormButton height={'53px'}
+                                    width={'198px'}
+                                    text={'Send request'}
+                        ></FormButton>
+                    </div>
+
+                    <div className={`${styles['buttonsStyle']} `}>
+                        <FormButton height={'53px'}
+                                    width={'198px'}
+                                    text={'Call'}
+                        ></FormButton>
+                    </div>
+
+                    <div className={`${styles['buttonsStyle']} `}>
+                        <FormButton height={'53px'}
+                                    width={'198px'}
+                                    text={'See profile'}
+                        ></FormButton>
+                    </div>
+                    <a className={`${styles['saveTextButton']} ${currentTheme.textColor}`}>
+                        save
+                    </a>
+                </div>
             </div>
         </div>
 
