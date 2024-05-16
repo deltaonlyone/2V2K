50bedc4ded85f1c3a0a0ec716c9c639005c6dc05
StopRey <skillful02@gmail.com>
2024-05-16 21:30:54 +0300

    added few things to page detailed photo

--- src/ProjectElements/Profile/PhotoDetailInfo/PhotoDetailInfo.js
+++ src/ProjectElements/Profile/PhotoDetailInfo/PhotoDetailInfo.js
@@ -10,6 +10,7 @@ import {FormButtonLink} from "../../Forms/FormButtonLink/FormButtonLink";
 import {FormButtonReverse} from "../../Forms/FormButtonReverse/FormButtonReverse";
 import {FormPhotographerSearchInfo} from "../../Forms/FormPhotographerSearchInfo/FormPhotographerSearchInfo";
 import {FormPlacePhotoPhotographer} from "../../Forms/FormPlacePhotoPhotographer/FormPlacePhotoPhotographer";
+import {SvgHeart, SvgSignOut} from "../../Forms/FormSvg/FormSvg";
 
 const PhotoDetailInfo = () => {
 
@@ -41,8 +42,61 @@ const PhotoDetailInfo = () => {
 
 
                 <div className={`${styles['containerDiv']} ${currentTheme.backgroundColor}`}>
+                    <div className={`${styles['userInfoDiv']}`}>
+                        <div className={`${styles['avatarUserDiv']}`}>
+                            <img className={styles['avatarUser']}
+                                 src={'https://imgv3.fotor.com/images/slider-image/Female-portrait-picture-enhanced-with-better-clarity-and-higher-quality-using-Fotors-free-online-AI-photo-enhancer.jpg'}
+                                 alt="User Avatar"/>
+                        </div>
+
+                        <a className={`${styles['userInfoTextName']} ${currentTheme.textColor}`}>
+                            Kyrylo Sydor
+                        </a>
+
+                        <a className={`${styles['userInfoTextUsername']} ${currentTheme.textColor}`}>
+                            @kyrylo_sydor
+                        </a>
 
 
+
+                    </div>
+
+                    <div className={`${styles['currentPhotoDiv']}`}>
+
+                        <img className={styles['currentPhoto']}
+                             src={'https://st2.depositphotos.com/1591133/8812/i/450/depositphotos_88120646-stock-photo-idyllic-summer-landscape-with-clear.jpg'}
+                             alt="User Avatar"/>
+
+
+                    </div>
+
+                    <div className={`${styles['buttonsDiv']}`}>
+
+                        <div className={`${styles['likeButton']}`}>
+
+                            <SvgHeart
+                                color={currentTheme.textColor === 'DarkTheme_textColor__65lH+' ? "white" : "black"}
+                                stroke={currentTheme.textColor === 'DarkTheme_textColor__65lH+' ? "white" : "black"}
+                                colorReverse={currentTheme.textColor === 'DarkTheme_textColor__65lH+' ? "black" : "white"}
+                                className={`${styles["svgTextLeft"]}`}
+                            />
+
+                            <a className={`${styles['likeText']} ${currentTheme.textColor}`}>
+                                Like
+                            </a>
+
+                        </div>
+
+
+                    </div>
+
+
+                    <div className={`${styles['commentsDiv']}`}>
+
+
+
+
+                    </div>
 
 
                 </div>
